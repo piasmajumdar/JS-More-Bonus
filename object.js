c50ede92db4ba206991bd17f9f642bf8f092a724
piasmajumdar <piasmajumdar@gmail.com>
2026-02-09 23:05:06 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'Sakib Khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friends: ['Apu', 'Raaj', 'Salman', 'aamir'],
+    car: {
+        brand: 'tesla',
+        price: 5000000,
+        made: 2025
+    }
+}
+
+console.log(student.car);
